b1d61115ea09b98d5abbfaf7c6dd59a4ef2de18a
Simon Voss <woss.simon@gmail.com>
2020-02-13 20:22:21 +0100

    map generation start

---
+++ Assets/Scripts/MapGeneration/LevelGeneration.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    public GameObject[] mapSectionBlueprints = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateLevel(3);
+    }
+
+    public void GenerateLevel(int criticalPathLength)
+    {
+        Vector3 spawnPos = Vector3.zero;
+
+        for (int i = 0; i < criticalPathLength; i++)
+        {
+            //Start
+            GameObject newSectionObject = Instantiate(mapSectionBlueprints[Random.Range(0, mapSectionBlueprints.Length)]);
+            newSectionObject.transform.position = spawnPos;
+
+
+            MapSection newSection = newSectionObject.GetComponent<MapSection>();
+
+            Direction newDirection = newSection.exitDirections[Random.Range(0, newSection.exitDirections.Count)];
+
+            switch (newDirection)
+            {
+                case Direction.Left:
+                    spawnPos += new Vector3(-newSection.sectionXSize, 0);
+                    break;
+                case Direction.Right:
+                    spawnPos += new Vector3(newSection.sectionXSize, 0);
+                    break;
+                case Direction.Up:
+                    spawnPos += new Vector3(0, newSection.sectionYSize);
+                    break;
+                case Direction.Down:
+                    spawnPos += new Vector3(0, -newSection.sectionYSize);
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MapGeneration/MapSection.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MapSection : MonoBehaviour
+{
+    public int sectionXSize = 10;
+    public int sectionYSize = 10;
+
+    public Tile[,] sectionTiles;
+
+    public Transform sectionExitParent = null;
+    public List<Tile> sectionExits = new List<Tile>();
+    public List<Direction> exitDirections = new List<Direction>();
+
+
+    private void Start()
+    {
+        InitializeSectionTiles();
+        SetSectionExits();
+    }
+
+    public void InitializeSectionTiles()
+    {
+        sectionTiles = new Tile[sectionXSize, sectionYSize];
+
+        for (int x = 0; x < sectionTiles.GetLength(0); x++)
+        {
+            for (int y = 0; y < sectionTiles.GetLength(1); y++)
+            {
+                sectionTiles[x, y] = new Tile(x, y);
+            }
+        }
+    }
+
+    public void SetSectionExits()
+    {
+        Transform[] sectionExitTransforms = sectionExitParent.GetComponentsInChildren<Transform>();
+        foreach (var item in sectionExitTransforms)
+        {
+            sectionExits.Add(GetClosestTile(item.localPosition.x, item.localPosition.y));
+        }
+    }
+
+    private Tile GetClosestTile(float x, float y)
+    {
+        int _x = Mathf.RoundToInt(x);
+        int _y = Mathf.RoundToInt(y);
+
+        foreach (var item in sectionTiles)
+        {
+            if (item.x == _x && item.y == _y)
+            {
+                return item;
+            }
+        }
+        Debug.LogError("Tile not found, this will cause unexpected behaviour");
+        return new Tile(10000, 10000);
+    }
+}
+
+[System.Serializable]
+public struct Tile
+{
+    public int x;
+    public int y;
+
+    public Tile(int _x, int _y)
+    {
+        x = _x;
+        y = _y;
+    }
+}
