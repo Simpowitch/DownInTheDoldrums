5684a8a3fc58303414248cadfe31b2fabcd83dc3
peanutpowah <samuel.zelenka@hotmail.com>
2020-02-19 14:03:11 +0100

    Equipment
    WeaponWheelSelection
    Script renaming ability = equipment

---
+++ Assets/Scripts/Player/CharacterInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterInteraction), typeof(CharacterMovement), typeof(CharacterWeaponSelect))]
+public class CharacterInput : MonoBehaviour
+{
+    CharacterInteraction interaction;
+    CharacterMovement movement;
+    CharacterWeaponSelect weaponSelect;
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        interaction = GetComponent<CharacterInteraction>();
+        movement = GetComponent<CharacterMovement>();
+        weaponSelect = GetComponent<CharacterWeaponSelect>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        interaction.InputCheck(Input.GetAxis("HorizontalSecondary"), Input.GetAxis("VerticalSecondary"));
+        movement.InputCheck(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        weaponSelect.InputCheck(Input.GetAxis("WeaponHorizontal"), Input.GetAxis("WeaponVertical"));
+    }
+
+
+
+}
